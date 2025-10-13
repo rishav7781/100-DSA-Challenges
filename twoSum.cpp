7c4998c56e4f700ee34e3b33538da0a0840d18d9
Rishav <guptarishav49@gmail.com>
2025-10-13 18:35:26 +0530

    two sum

---
+++ twoSum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int rem=target-nums[i];
+
+            if(mp.find(rem)!=mp.end()){
+                return {i,mp[rem]};
+            }else{
+                mp[nums[i]]=i;
+            }
+        }
+        return {-1,-1};
+    }
+};
